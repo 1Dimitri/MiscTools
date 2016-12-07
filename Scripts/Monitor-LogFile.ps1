adc0740bf8fca1e59c2d09037ef7fbb97299a418
1Dimitri <git.dimitri@janczak.net>
2016-12-07 16:18:53 +0100

    Bug correction

--- Scripts/Monitor-LogFile.ps1
+++ Scripts/Monitor-LogFile.ps1
@@ -7,9 +7,11 @@ $recurse = $true # sub directories?
 $fsw = New-Object IO.FileSystemWatcher $folder, $filter -Property @{IncludeSubdirectories = $recurse;NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'}
 
 Register-ObjectEvent $fsw Changed -SourceIdentifier LogMonitor -Action {
+
 $path = $Event.SourceEventArgs.FullPath
 $changeType = $Event.SourceEventArgs.ChangeType
 $timeStamp = $Event.TimeGenerated
+Write-VErbose "[$timeStamp] $changeType $path"
 if ($fileptr -eq $null) {
  $fileptr=@{}
 }
@@ -17,22 +19,34 @@ $fs = New-Object System.IO.FileStream($path,"Open","Read","ReadWrite",8,"None")
 #ReadWrite as 4th parameter -> do not block file
 $sr = New-Object System.IO.StreamReader($fs)
 $pos = $fileptr[$path]
+
 if ($pos) {
+  Write-Verbose "This file was already read until $pos"
   $sr.BaseStream.Seek($pos,"Begin")
 
   
 }
+ Write-Verbose 'Reading lines'
   $line = $sr.ReadLine()
+  WRite-Verbose "Line: [$line]"
+  
   while ($line -ne $null) {
-  $found = line | Select-String 'ERROR' -SimpleMatch -Quiet
+  $found = $line | Select-String 'ERROR' -SimpleMatch -Quiet
   if ($found) {
       # Send Alert for each pattern found
+	  Write-Verbose 'Pattern found'
+	  #change source as appropriate, otherwise you'll get error
 	  Write-EventLog -LogName Application -Source WatchDogSript -Message "pattern found in $path at position $($sr.BaseStream.Position)" -EventId 777 -EntryType information
 	}
+	else {
+	 Write-Verbose 'Pattern not found'
+	}
+	  $line = $sr.ReadLine()
   }
   
   $fileptr[$path]=$sr.BaseStream.Position
-
+  $sr.Close()
+  $fs.Close()
 }
 
 # To stop the monitoring, run the following commands:
